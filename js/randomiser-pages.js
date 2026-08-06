1995dd99a2972452b5656ed7d51ab8bc6ac6330d
jaybaeta <joaquimbaeta@protonmail.com>
2026-08-06 11:18:19 +0700

    make randomiser work on non-main pages

---
+++ js/randomiser-pages.js
@@ -0,0 +1,53 @@
+const imageArray = [
+  '../images/jumbotron/borobudur-relief.avif',
+  '../images/jumbotron/plants.avif',
+  '../images/jumbotron/venice-gull.avif',
+  '../images/jumbotron/wave.avif',
+  '../images/jumbotron/colosseum.avif',
+  '../images/jumbotron/forest.avif',
+  '../images/jumbotron/mountain.avif',
+  '../images/jumbotron/supri-walk.avif',
+  '../images/jumbotron/swimming-with-whale-shark.avif',
+  '../images/jumbotron/wave-crash.avif',
+  '../images/jumbotron/wave-crash-closeup.avif'
+];
+
+const fontClasses = [
+  'font-atlas-regular',
+  'font-deliria-nouveau',
+  'font-genzsch-et-heyse-regular',
+  'font-hazelnut-pro-black',
+  'font-isidora-light',
+  'font-le-Havre-layers-primary',
+  'font-macaronisans-regular',
+  'font-mazzard-soft-m-light',
+  'font-sanguine',
+  'font-vendura-semibold'
+];
+
+const randPath = imageArray[Math.floor(Math.random() * imageArray.length)];
+const randFont = fontClasses[Math.floor(Math.random() * fontClasses.length)];
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('#jumbotron h1, .navbar-brand').forEach(el => {
+    el.classList.add(randFont);
+  });
+
+  const jumbotronImage = document.getElementById('jumbotron-image');
+
+  if (jumbotronImage) {
+    jumbotronImage.onload = () => {
+      jumbotronImage.classList.add('loaded');
+    };
+    jumbotronImage.onerror = () => {
+      console.error(`Couldn't load ${randPath}`);
+    };
+    jumbotronImage.src = randPath;
+  }
+
+  const footer = document.getElementById('img-footer');
+
+  if (footer) {
+    footer.style.background = `url(${randPath}) center center no-repeat`;
+  }
+});
